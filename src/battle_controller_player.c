5f90442e15dbc61ee2f13ece911b612325218ff7
Snaid1 <jollymormongiant@gmail.com>
2022-10-23 23:30:14 -0600

    show category icon on move info summary screen.

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -2968,6 +2968,7 @@ static void MoveSelectionDisplaySplitIcon(void){
 	moveInfo = (struct ChooseMoveStruct*)(&gBattleBufferA[gActiveBattler][4]);
 	icon = GetBattleMoveCategory(moveInfo->moves[gMoveSelectionCursor[gActiveBattler]]);
 	LoadPalette(sSplitIcons_Pal, 10 * 0x10, 0x20);
+    FillWindowPixelBuffer(B_WIN_DUMMY, PIXEL_FILL(7));
 	BlitBitmapToWindow(B_WIN_DUMMY, sSplitIcons_Gfx + 0x80 * icon, 0, 0, 16, 16);
 	PutWindowTilemap(B_WIN_DUMMY);
 	CopyWindowToVram(B_WIN_DUMMY, 3);
